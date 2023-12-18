0da6f10d96ea2312887212f3ced2257c7d4ee97b
dylans1lk <151115198+dylans1lk@users.noreply.github.com>
2023-12-17 23:59:52 -0500

    Add files via upload

---
+++ js/scrabble.js
@@ -0,0 +1,71 @@
+// Dylan Silk - dylan_silk@student.uml.edu
+// Borrowed from the given scrabble pieces associatve array
+var ScrabbleTiles = [] ;
+ScrabbleTiles["A"] = { "value" : 1,  "original-distribution" : 9,  "number-remaining" : 9  } ;
+ScrabbleTiles["B"] = { "value" : 3,  "original-distribution" : 2,  "number-remaining" : 2  } ;
+ScrabbleTiles["C"] = { "value" : 3,  "original-distribution" : 2,  "number-remaining" : 2  } ;
+ScrabbleTiles["D"] = { "value" : 2,  "original-distribution" : 4,  "number-remaining" : 4  } ;
+ScrabbleTiles["E"] = { "value" : 1,  "original-distribution" : 12, "number-remaining" : 12 } ;
+ScrabbleTiles["F"] = { "value" : 4,  "original-distribution" : 2,  "number-remaining" : 2  } ;
+ScrabbleTiles["G"] = { "value" : 2,  "original-distribution" : 3,  "number-remaining" : 3  } ;
+ScrabbleTiles["H"] = { "value" : 4,  "original-distribution" : 2,  "number-remaining" : 2  } ;
+ScrabbleTiles["I"] = { "value" : 1,  "original-distribution" : 9,  "number-remaining" : 9  } ;
+ScrabbleTiles["J"] = { "value" : 8,  "original-distribution" : 1,  "number-remaining" : 1  } ;
+ScrabbleTiles["K"] = { "value" : 5,  "original-distribution" : 1,  "number-remaining" : 1  } ;
+ScrabbleTiles["L"] = { "value" : 1,  "original-distribution" : 4,  "number-remaining" : 4  } ;
+ScrabbleTiles["M"] = { "value" : 3,  "original-distribution" : 2,  "number-remaining" : 2  } ;
+ScrabbleTiles["N"] = { "value" : 1,  "original-distribution" : 6,  "number-remaining" : 6  } ;
+ScrabbleTiles["O"] = { "value" : 1,  "original-distribution" : 8,  "number-remaining" : 8  } ;
+ScrabbleTiles["P"] = { "value" : 3,  "original-distribution" : 2,  "number-remaining" : 2  } ;
+ScrabbleTiles["Q"] = { "value" : 10, "original-distribution" : 1,  "number-remaining" : 1  } ;
+ScrabbleTiles["R"] = { "value" : 1,  "original-distribution" : 6,  "number-remaining" : 6  } ;
+ScrabbleTiles["S"] = { "value" : 1,  "original-distribution" : 4,  "number-remaining" : 4  } ;
+ScrabbleTiles["T"] = { "value" : 1,  "original-distribution" : 6,  "number-remaining" : 6  } ;
+ScrabbleTiles["U"] = { "value" : 1,  "original-distribution" : 4,  "number-remaining" : 4  } ;
+ScrabbleTiles["V"] = { "value" : 4,  "original-distribution" : 2,  "number-remaining" : 2  } ;
+ScrabbleTiles["W"] = { "value" : 4,  "original-distribution" : 2,  "number-remaining" : 2  } ;
+ScrabbleTiles["X"] = { "value" : 8,  "original-distribution" : 1,  "number-remaining" : 1  } ;
+ScrabbleTiles["Y"] = { "value" : 4,  "original-distribution" : 2,  "number-remaining" : 2  } ;
+ScrabbleTiles["Z"] = { "value" : 10, "original-distribution" : 1,  "number-remaining" : 1  } ;
+ScrabbleTiles["_"] = { "value" : 0,  "original-distribution" : 2,  "number-remaining" : 2  } ;
+
+// make id: draggable ... draggable
+$( function() {
+  $( "#draggable" ).draggable();
+});
+
+// makes all the board tiles have their own droppable...
+$( function () {
+  $( "#board div" ).droppable({
+    accept: "#draggable",
+    drop: function(event, ui) {
+      $(this).append(ui.helper);
+      ui.helper.css({top: 0, left: 0});
+    }
+  });
+});
+
+// generate a random tile from one of the array ScrabbleTiles
+function randTile() {
+  var key = Object.keys(ScrabbleTiles);
+  return key[Math.floor(Math.random() * key.length)];
+}
+
+// create a tile inside the #tiles div
+function createTile(tile) {
+  var newTile = $("<div id='draggable' class='draggable ui-widget-content'></div>")
+    .data("tile", tile)
+    .append("<img src='./imgs/scrabble_tiles/Scrabble_Tile_" + tile + ".jpg' alt='" + tile + "'>"); // appends to the file path
+
+    newTile.draggable();
+
+    return newTile;
+}
+
+// create 7 tiles on the game start
+$(document).ready(function() {
+  for (var i = 0; i < 7; i++) {
+    var tile = createTile(randTile());
+    tile.appendTo("#tiles");
+  }
+});
---
+++ readme.txt
@@ -0,0 +1,4 @@
+Dylan Silk, dylan_silk@student.uml.edu
+
+I only got as far as randomizing the tiles, and making them snap to the board.
+I put my sources in my index.html file.
